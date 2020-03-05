71095e53953833649682b610b2524329c5added3
aleffarias <aaof@ic.ufal.br>
2020-03-05 14:47:44 -0300

    erro

--- src/screens/registerMed/styles.js
+++ src/screens/registerMed/styles.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import {StyleSheet} from 'react-native';
 import {colors} from '../../styles';
 
@@ -53,3 +54,48 @@ const styles = StyleSheet.create({
 });
 
 export default styles;
+=======
+import {StyleSheet} from 'react-native';
+import {colors} from '../../styles';
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.primary,
+    flex: 1,
+    justifyContent: 'center',
+  },
+
+  // Safe Area
+  safeArea: {
+    minHeight: 8,
+  },
+
+  // Header Tittle
+  headerCard: {
+    justifyContent: 'center',
+    fontWeight: 'bold',
+  },
+
+  // Button next
+  footerContainer: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+  },
+  footerControl: {
+    marginHorizontal: 4,
+  },
+
+  // Cards
+  cardMain: {
+    borderRadius: 20,
+  },
+  cardContainer: {
+    backgroundColor: colors.grayBackground,
+    borderColor: colors.white,
+    borderRadius: 30,
+    marginBottom: 20,
+  },
+});
+
+export default styles;
+>>>>>>> parent of 4f50b44... Update for registerMed
