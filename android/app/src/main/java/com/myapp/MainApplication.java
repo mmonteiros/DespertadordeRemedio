19bcdcc8fa7f2793ed91349fa3265fd1e850a93d
aleffarias <aaof@ic.ufal.br>
2020-07-16 03:52:37 -0300

    Update: background timer

--- android/app/src/main/java/com/myapp/MainApplication.java
+++ android/app/src/main/java/com/myapp/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.ocetnik.timer.BackgroundTimerPackage;
 //Firebase Imports
 import io.invertase.firebase.auth.RNFirebaseAuthPackage;
 import io.invertase.firebase.firestore.RNFirebaseFirestorePackage;
